763c281d60f879c380f374e7558174864570b2d0
kenmaz <kenmaz@mercari.com>
2019-07-01 12:36:00 +0900

    だめでした

--- navbar_test/AppDelegate.swift
+++ navbar_test/AppDelegate.swift
@@ -22,9 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // UINavigationBar
-        UINavigationBar.appearance().tintColor = UIColor.gray
+        UINavigationBar.appearance().tintColor = UIColor.black
         UINavigationBar.appearance().titleTextAttributes = [
-            .foregroundColor: UIColor.gray,
+            .foregroundColor: UIColor.black,
             .font: UIFont.systemFont(ofSize: 17, weight: .semibold)
         ]
         UINavigationBar.appearance().barTintColor = UIColor.white
